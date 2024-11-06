30d24a6ebc3afeea5d52f702089c930179e2c6f5
cwagnerJLS <cwagner@jlsautomation.com>
2024-11-06 16:08:33 -0500

    Test

--- inventory_system/inventory_system/settings.py
+++ inventory_system/inventory_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=)&i%gn%h#-5r_+@(r8e-!#il^c)szc&maalmnb34=7*lxenq%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.6.91']
 
 # Application definition
 
