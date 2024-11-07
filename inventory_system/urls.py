82e20c2a007b053415d002afd9d877c8ff41435a
cwagnerJLS <cwagner@jlsautomation.com>
2024-11-07 16:11:15 -0500

    Refactor

--- inventory_system/urls.py
+++ inventory_system/urls.py
@@ -1,3 +1,5 @@
+# inventory_system/urls.py
+
 from django.contrib import admin
 from django.urls import path
 from samples import views
@@ -5,6 +7,7 @@ from samples import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.create_sample, name='home'),
+    path('create_sample/', views.create_sample, name='create_sample'),  # Re-added URL pattern
     path('update_sample_location/', views.update_sample_location, name='update_sample_location'),
     path('delete_samples/', views.delete_samples, name='delete_samples'),
     path('handle_print_request/', views.handle_print_request, name='handle_print_request'),
