c8abf5f804a67ea27a7535b90ab97f8d72fa3b45
cwagnerJLS <cwagner@jlsautomation.com>
2024-11-06 20:45:55 -0500

    Got rid of nested structure

--- samples/utils.py
+++ samples/utils.py
@@ -8,6 +8,6 @@ def get_unique_values(data, key):
     return list({record[key] for record in data})
 
 # Example usage
-file_path = r"C:\Users\cwagner\PycharmProjects\Sample_Database\Sample Inventory.xlsx"
+file_path = r"/Sample Inventory.xlsx"
 excel_data = read_excel_data(file_path)
 unique_customers = get_unique_values(excel_data, 'Customer')
