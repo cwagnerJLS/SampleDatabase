064ca18af58d394a98b989e4e79817341a99b2fd
cwagnerJLS <cwagner@jlsautomation.com>
2024-11-08 10:36:44 -0500

    Test

--- samples/models.py
+++ samples/models.py
@@ -1,6 +1,9 @@
 import random
 from django.db import models
 
+def generate_unique_id():
+    return random.randint(1000, 9999)
+
 class Sample(models.Model):
     unique_id = models.PositiveIntegerField(unique=True, editable=False)
     date_received = models.DateField()
@@ -17,7 +20,7 @@ class Sample(models.Model):
             ('Walk-in Fridge', 'Walk-in Fridge'),
             ('Walk-in Freezer', 'Walk-in Freezer'),
             ('Dry Food Storage', 'Dry Food Storage'),
-            ('Empty Case Storage', 'Empty Case Storage')
+            ('Empty Case Storage', 'Empty Case Storage'),
         ]
     )
     quantity = models.IntegerField(default=1)
@@ -27,16 +30,12 @@ class Sample(models.Model):
     def save(self, *args, **kwargs):
         if not self.unique_id:
             for _ in range(100):
-                self.unique_id = self.generate_unique_id()
+                self.unique_id = generate_unique_id()
                 if not Sample.objects.filter(unique_id=self.unique_id).exists():
                     break
             else:
                 raise ValueError("Could not generate a unique ID after 100 attempts.")
         super().save(*args, **kwargs)
-
-    @staticmethod
-    def generate_unique_id():
-        return random.randint(1000, 9999)
 
     def __str__(self):
         return f"Sample {self.unique_id} - {self.customer}"
