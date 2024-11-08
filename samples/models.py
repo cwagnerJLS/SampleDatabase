38d7d5ccf836ce44b5f884378cb652420b14fb1c
cwagnerJLS <cwagner@jlsautomation.com>
2024-11-08 10:21:51 -0500

    Test

--- samples/models.py
+++ samples/models.py
@@ -1,9 +1,14 @@
 import random
 from django.db import models
-from django.db import transaction, IntegrityError
+
+def generate_unique_id():
+    while True:
+        new_id = random.randint(1000, 9999)
+        if not Sample.objects.filter(unique_id=new_id).exists():
+            return new_id
 
 class Sample(models.Model):
-    unique_id = models.PositiveIntegerField(unique=True, editable=False)
+    unique_id = models.PositiveIntegerField(default=generate_unique_id, unique=True)
     date_received = models.DateField()
     customer = models.CharField(max_length=255)
     opportunity_number = models.CharField(max_length=255)
@@ -16,31 +21,14 @@ class Sample(models.Model):
             ('Test Lab Fridge', 'Test Lab Fridge'),
             ('Test Lab Freezer', 'Test Lab Freezer'),
             ('Walk-in Fridge', 'Walk-in Fridge'),
-            ('Walk-in Freezer', 'Walk-in Freezer')
+            ('Walk-in Freezer', 'Walk-in Freezer'),
+            ('Dry Food Storage', 'Dry Food Storage'),
+            ('Empty Case Storage', 'Empty Case Storage'),
         ]
     )
     quantity = models.IntegerField(default=1)
     description = models.TextField(default="No description")
     audit = models.BooleanField(default=False)
-
-def save(self, *args, **kwargs):
-    if not self.unique_id:
-        for _ in range(100):
-            self.unique_id = self.generate_unique_id()
-            try:
-                with transaction.atomic():
-                    super().save(*args, **kwargs)
-                break
-            except IntegrityError:
-                continue
-        else:
-            raise IntegrityError("Could not generate a unique ID after 100 attempts.")
-    else:
-        super().save(*args, **kwargs)
-
-@staticmethod
-def generate_unique_id():
-    return random.randint(1000, 9999)
 
 def __str__(self):
     return f"Sample {self.unique_id} - {self.customer}"
